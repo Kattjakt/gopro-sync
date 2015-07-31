e6eca6ca88be83e9daa0f60b63f631a27fa9ab9e
Leo Sandström <leosand97@gmail.com>
2015-07-31 03:23:43 +0200

    kinda working

--- index.js
+++ index.js
@@ -6,10 +6,121 @@ var fs = require('fs');
 var http = require('http');
 var wait = require('wait.for');
 var async = require('async');
+var cliArgs = require("command-line-args");
+
+//var spinner = new spinner.Spinner('%s');
+
+var cli = cliArgs([
+    { name: "ip", type: String, alias: "ip", description: "Choose a custom device IP" },
+    { name: "directory", type: String, alias: "d", description: "Output directory for synced media" }
+]);
 
 
-var spinner = new spinner.Spinner('%s');
+var Gopro = function() {
+  var args = cli.parse();
+  this.options = {
+    path: args.directory  || 'out/',
+    ip:   args.ip         || 'http://10.5.5.9',
+    allowedFiles: ['JPG', 'MP4']
+  };
 
+  this.fetch = function(filename, callback) {
+    var self = this;
+
+    var file = fs.createWriteStream(self.options.path + filename);
+    var req = request(self.options.ip + '/videos/DCIM/100GOPRO/' + filename, function(response) {
+      response.pipe(file);
+    });
+
+    //req.pipe(out);
+    req.on('error', function() {
+      console.log('REQUEST ERROR'.red);
+    });
+
+    callback();
+  };
+};
+
+Gopro.prototype.sync = function() {
+    var self = this;
+
+    console.log('output directory set to: ' + self.options.path.yellow);
+    if (!fs.existsSync(self.options.path)) {
+      fs.mkdirSync(self.options.path);
+    }
+
+    // Get a list of all files currently stored on the Gopro unit
+    request(self.options.ip + '/videos/DCIM/100GOPRO/', function(error, resp, body) {
+      if (error) {
+        return console.error(error.message);
+      }
+
+      var html = cheerio.load(body);
+      var links = html('a.link[href]');
+
+      wait.launchFiber(function() {
+        for (var i = 0; i < links.length; i++) {
+
+          // Get the full filename and extension of the current file
+          var filename = links[i].attribs.href;
+          var extension = filename.split('.').pop();
+
+          // Check if the extension is in the allowed list
+          if (self.options.allowedFiles.indexOf(extension) == -1) {
+            console.log('banned extension, skipping ' + filename.yellow);
+          } else {
+            // If not, download the media
+            //self.fetch(filename);
+            //wait.for(self.fetch, filename);
+            console.log('downloading: ' + filename.yellow);
+
+
+            var out = fs.createWriteStream(self.options.path + filename);
+
+            var req = wait.for(request, self.options.ip + '/videos/DCIM/100GOPRO/' + filename);
+            fs.writeFile(self.options.path + filename, JSON.stringify(req.body), function(err) {
+              if (err) return console.log(err);
+            });
+
+            //req.pipe(out);
+            req.on('error', function() {
+              console.log('REQUEST ERROR'.red);
+            });
+            
+
+          }
+        }
+      });
+    });
+  };
+
+/*
+    wait.launchFiber(function() {
+      var body = wait.for(request, self.options.ip + '/videos/DCIM/100GOPRO');
+      var html = cheerio.load(body);
+      var links = html('a.link[href]');
+
+      links.each(function(i) {
+        var filename = links[i].attribs.href;
+        var extension = filename.split('.').pop();
+
+        // Check if the extension is in the allowed list
+        if (self.options.allowedFiles.indexOf(extension) != -1) {
+          console.log('banned extension, skipping ' + filename.yellow);
+        } else {
+          self.fetch(filename);
+
+        }
+
+
+      });
+    });
+};
+*/
+var g = new Gopro();
+g.sync();
+
+/*
 // Check if the user provided a custom output directory
 var path = process.argv[2];
 if (path) {
@@ -26,12 +137,12 @@ if (!fs.existsSync(path)) {
     console.log('ok'.green);
 }
 
-process.stdout.write("connecting to GoPro unit (10.5.5.9)... ")
+process.stdout.write("connecting to GoPro unit (10.5.5.9)... ");
 request('http://10.5.5.9/videos/DCIM/100GOPRO', function(error, response, body) {
     if (error) {
         return console.error(('unable to connect [' + error.code + ']').red);
-    } 
-    
+    }
+
     console.log('ok'.green);
     process.stdout.write('parsing html to get file paths... ');
 
@@ -49,33 +160,38 @@ request('http://10.5.5.9/videos/DCIM/100GOPRO', function(error, response, body) 
     },
     function (next, cb) {
         filename = links[i].attribs.href;
-        if (filename.split(".") != 'THM' &&
-            filename.split(".") != 'LRV') {
-            if (fs.readdirSync(path).indexOf(filename) != -1) {
-                console.log('file ' + filename.yellow + ' found in output directory, skipping');
-                i++;
-                next();
-            } else {
-                console.log('starting download of ' + filename.yellow + '... ');
+        extension = filename.split('.').pop();
 
-                var file = fs.createWriteStream(path + '/' + filename);
-                var request = http.get("http://10.5.5.9/videos/DCIM/100GOPRO/" + filename, function(response) {
-                    response.pipe(file);
-                    file.on('finish', function() {
-                        file.close(cb);
-                        i++;
-                        next();
-                    });
-                }).on('error', function(err) {
-                    fs.unlink(path + '/' + filename); 
-                    if (cb) cb(err.message);
-                }); 
-            }
+        if (extension == 'LRV' || extension == 'THM') {
+
+        }
+
+
+
+        if (fs.readdirSync(path).indexOf(filename) != -1) {
+            console.log('file ' + filename.yellow + ' found in output directory, skipping');
+            i++;
+            next();
+        } else {
+            console.log('starting download of ' + filename.yellow + '... ');
+
+            var file = fs.createWriteStream(path + '/' + filename);
+            var request = http.get("http://10.5.5.9/videos/DCIM/100GOPRO/" + filename, function(response) {
+                response.pipe(file);
+                file.on('finish', function() {
+                    file.close(cb);
+                    i++;
+                    next();
+                });
+            }).on('error', function(err) {
+                fs.unlink(path + '/' + filename);
+                if (cb) cb(err.message);
+            });
         }
     },
     function (err) {
         console.log('\ndone!'.green);
-    });    
+    });
 });
 
 
@@ -85,77 +201,14 @@ function transfer(filename, outputdir) {
         console.log('file ' + filename.yellow + ' found in output directory, skipping');
         return false;
     }
-    
+
     console.log('starting download of ' + filename.yellow + '... ');
-    
+
     var file = fs.createWriteStream(outputdir + '/' + filename);
     http.get("http://10.5.5.9/videos/DCIM/100GOPRO/" + filename, function(response) {
         response.pipe(file);
     });
-    
-    
+
     console.log("DONE?");
-    
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
